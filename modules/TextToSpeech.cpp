ead474cd849d2e7f23cbf1b32e505ad2cdb4cb1d
HappyPotatoHead <jimmyding9719@gmail.com>
2025-09-19 10:17:35 +0800

    corrected test to speech

--- modules/TextToSpeech.cpp
+++ modules/TextToSpeech.cpp
@@ -5,8 +5,8 @@
 namespace NavigationVI{
     void TextToSpeech::speak(const std::string& text){
         #ifdef _WIN32
-            std::string command = "PowerShell -Command \"Add-Type -AssemblyName System.Speeh; " 
-                                 "(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('" + text "');\"";
+            std::string command = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; " 
+                                "(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('" + text + "');\"";
         #elif __APPLE__
             std::string command = "say \"" + text + "\"";
         #else  
